cb735886fcee4cbf85e63ddd2f3140fca34bf702
choieunchong <zlqh1997@gmail.com>
2022-12-26 17:02:48 +0900

    Stencil

---
+++ Stencil/widget.h
@@ -0,0 +1,29 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QtOpenGL>
+
+class Widget : public QOpenGLWidget, protected QOpenGLFunctions
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+    GLfloat nx, ny;
+    GLboolean bScissor;
+    GLboolean bStencil;
+    GLboolean bEqual;
+
+
+private:
+    void initializeGL();
+    void paintGL();
+    void resizeGL(int w, int h);
+
+protected:
+    void keyPressEvent(QKeyEvent* event);
+};
+#endif // WIDGET_H
