418c4e9d7bf7072c18921471fa6ae325c9e60a96
choieunchong <zlqh1997@gmail.com>
2022-12-26 18:10:57 +0900

    LuightModel

---
+++ LuightModel/widget.h
@@ -0,0 +1,25 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QtOpenGL>
+#include <GL/glut.h>
+class Widget : public QOpenGLWidget, protected QOpenGLFunctions
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+    GLfloat xAngle, yAngle, zAngle;
+    GLfloat red, green, blue;
+
+private:
+    void initializeGL();
+    void paintGL();
+    void resizeGL(int w, int h);
+protected:
+    void keyPressEvent(QKeyEvent* event);
+};
+#endif // WIDGET_H
